14c4cd801ccd1f465d25f04228e8c04c9c69d5a1
IgnReLoaD <ortiz.ignasi@gmail.com>
2023-02-13 23:54:55 +0100

    tratando de hacer 2 combos nidadas

--- resources/views/game/edit.blade.php
+++ resources/views/game/edit.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.plantillabase');
 @section('contenido')
 
+<div class="container" style="border: solid 1px green; background-color: #F0FFF0; background-image: url('.resources/assets/cesped.jpg'">  
+<p></p>
 <h2>Vista Editar un Partit</h2>
 <form action="/games/{{$game->id}}" method="post" >    
     @csrf 
@@ -42,7 +44,9 @@
             <label for="cmbHomeTeam" class="form-label">Equip local</label>
             <select id="cmbHomeTeam" class="form-control" style="width:200px" name="cmbHomeTeam" tabindex="5">
                 @foreach($teams as $team)
-                    <option value="{{$team->id}}">{{$team->name}}</option>
+                    @if ($team->id_club == $club->id)
+                        <option value="{{$team->id}}">{{$team->name}}</option>
+                    @endif
                 @endforeach
             </select>
         </div> 
@@ -68,7 +72,9 @@
             <label for="cmbAwayTeam" class="form-label">Equip visitant</label>
             <select id="cmbAwayTeam" class="form-control" name="cmbAwayTeam" style="width:200px" tabindex="9">
                 @foreach($teams as $team)
-                    <option value="{{$team->id}}">{{$team->name}}</option>
+                    @if ($team->id_club == $club->id)
+                        <option value="{{$team->id}}">{{$team->name}}</option>
+                    @endif
                 @endforeach
             </select>
         </div>          
@@ -83,9 +89,38 @@
 <hr>
     <!-- LLISTA D'ACCIONS D'AQUEST PARTIT  -->
     <div class="container mt-6">        
-        <a href="/games/{{$game->id}}/actions" class="btn btn-success">Veure el minut a minut (accions)... [opció no disponible]</a>
+        <!-- <a href="/games/{{$game->id}}/actions" class="btn btn-success">Veure el minut a minut (accions)... [opció no disponible]</a> -->
+        <a href="#" class="btn btn-success">Veure el minut a minut (accions)... [opció no disponible]</a>
         <!-- @csrf   -->
         <!-- @yield('teamsList') -->
     </div>    
 
+    <p></p>
+
+</div>
+
+
+    <script>
+        var cmbChange = document.getElementById("cmbHomeClub");        
+        cmbChange.addEventListener("change", function() {
+            console.log(cmbChange.value);
+            alert(cmbChange.value);
+            document.getElementById("cmbHomeTeam").innerHTML = foreach(teams as team){if(team->club_id == cmbChange.value) print("<option value="{{$team->id}}">{{$team->name}}</option>") }
+        });
+
+    // cmbClub = document.getElementById('cmbHomeClub');
+    // cmbClub.onchange(){
+    //     alert('change');
+    // }
+
+    // $(function(){
+    //     $('#cmbHomeClub').on('change', onSelectClubChange);
+    // });
+
+    // function onSelectClubChange(){
+    //     var club_id = $(this).val();
+    //     alert(club_id);
+    // }
+
+    </script>
 @endsection
